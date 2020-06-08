feee2fd577dd21ddf390eac20e7682e7e98795e4
rubensGouveia <rubensgui.gouveia@gmail.com>
2020-06-08 19:19:40 -0300

    Updates

---
+++ precache-manifest.27f21d7d4c83c6606ca766064535d706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd82170800f751aaabc772e9e5f6217",
+    "url": "/desafiofrontend-deploy/index.html"
+  },
+  {
+    "revision": "569719344631d5467055",
+    "url": "/desafiofrontend-deploy/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "8bdc17c714f3b8a87dbe",
+    "url": "/desafiofrontend-deploy/static/css/main.505b8cad.chunk.css"
+  },
+  {
+    "revision": "569719344631d5467055",
+    "url": "/desafiofrontend-deploy/static/js/2.04139f7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/desafiofrontend-deploy/static/js/2.04139f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdc17c714f3b8a87dbe",
+    "url": "/desafiofrontend-deploy/static/js/main.4acbdb4a.chunk.js"
+  },
+  {
+    "revision": "f6aa80dbf79913dcce5d",
+    "url": "/desafiofrontend-deploy/static/js/runtime-main.22eb677b.js"
+  }
+]);
